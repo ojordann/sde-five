38537a5982336b4886a89e76dff506efa47d8efc
Osinachi Njoku <osinachi.njoku@gmail.com>
2019-10-23 12:44:41 +1100

    init commit

---
+++ src/App.svelte
@@ -0,0 +1,13 @@
+<script>
+	export let name;
+</script>
+
+<style>
+	h1 {
+		color: purple;
+	}
+</style>
+
+<h1>Hello {name}!</h1>
+
+<p>This is a test Ansible Web application by <a href="https://njoku.dev">Osinachi Njoku</a>, built with <a href="https://svelte.dev/">svelte</a>!</p>
---
+++ src/main.js
@@ -0,0 +1,10 @@
+import App from './App.svelte';
+
+const app = new App({
+	target: document.body,
+	props: {
+		name: 'Software Deployment and Evolution Task 5',
+	},
+});
+
+export default app;
